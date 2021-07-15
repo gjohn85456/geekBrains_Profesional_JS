0d3654d01b6da5da8ae565e035c63f3671f30022
Evgeniy <john85456@ya.ru>
2021-07-15 21:04:08 +0500

    Task-2.1

--- index_js.js
+++ index_js.js
@@ -1,5 +1,5 @@
 'use strict'
-//Задание 1
+//Задание 2
 // const goods = [
 //     { title: 'Down jacket', price: 100, description: 'insulated jacket' }
 // ];
@@ -24,11 +24,12 @@
 // renderGoodsList();
 
 class Good {
-    constructor(title, price, description, urlImg) {
+    constructor(title, price, urlImg, description) {
         this._title = title;
         this._price = price;
-        this._description = description;
         this._urlImg = urlImg;
+        this._description = description;
+
     }
     render() {
         return `
@@ -45,8 +46,9 @@ class Good {
 }
 
 class GoodInCart extends Good {
-    constructor(urlImg, title, price, color, size, quantity = 1) {
-        super(urlImg, title, price);
+
+    constructor(title, price, urlImg, color, size, quantity = 1) {
+        super(title, price, urlImg);
         this._color = color;
         this._size = size;
         this._quantity = quantity;
@@ -58,9 +60,9 @@ class GoodInCart extends Good {
             <img src="${this._urlImg}" alt="imgItem-2_cart">
             <div class="mainCart_left_card-1_text">
                 <h2>${this._title}</h2>
-                <p>Price: <span class="mainCart_left_card-1_text_price">$${this_.price}</span> </p>
-                <p>Color: <span class="mainCart_left_card-1_text_color">${this_.color}</span> </p>
-                <p>Size: <span class="mainCart_left_card-1_text_size">${this_.size}</span> </p>
+                <p>Price: <span class="mainCart_left_card-1_text_price">$${this._price}</span> </p>
+                <p>Color: <span class="mainCart_left_card-1_text_color">${this._color}</span> </p>
+                <p>Size: <span class="mainCart_left_card-1_text_size">${this._size}</span> </p>
                 <p>Quantity:&nbsp;&nbsp; <span class="mainCart_left_card-1_text_quantity">${this._quantity}</span></p>
                 <img src="img/imgClose.png" alt="imgClose">
             </div>   
@@ -70,18 +72,28 @@ class GoodInCart extends Good {
 }
 
 class GoodsList {
-    constructor(goods) {
+    constructor(goods, container) {
         this._goods = goods;
-        this._$goodsListContainer = document.querySelector('.b-catalog');
+        this._$goodsListContainer = container;
     }
 
     renderGoodList() {
         let goodsList = this._goods.map(
             item => item.render()).join(' ');
-        this._$goodsListContainer.insertAdjacentHTML('beforeend', goodsList);
+        this._$goodsListContainer.insertAdjacentHTML('afterbegin', goodsList);
+    }
+    totalAmount() {
+        let total = 0;
+        this._goods.forEach(item => {
+            total = total + item._price * item._quantity;
+            console.log(item);
+        });
+        console.log(total);
+        const $totalAmount = document.querySelector('.totalAmount');
+        $totalAmount.textContent = total;
     }
 }
-//Создал массив данных для формирования карточек
+//Создал массив данных для формирования карточек index.html
 //title, price, description, url
 const arrayData = [];
 arrayData[0] = ["Men's Packable Down Puffer Jacket",
@@ -114,16 +126,23 @@ arrayData[5] = ["Green Filafil Shirt",
     "Refined womens' business dress shirt. Relaxed Fit: Our most generous fit sits farthest from the body Crafted with a very special fabric that lends a fun, preppy vibe to any look.",
     "img/imgCard-6.png",
 ];
-
+//массив данных для Cart.html
 //url, title, price, color, size, quantity
-// const arrayDataCart = [];
-// arrayDataCart[0] = ["img/imgItem-1_cart.png",
-//     "Men's Fashion Hoodies",
-//     125,
-//     "Red",
-//     "M",
-//     2
-// ];
+const arrayDataCart = [];
+arrayDataCart[0] = ["img/imgItem-1_cart.png",
+    "Men's Fashion Hoodies",
+    125,
+    "Blue",
+    "M",
+    2
+];
+arrayDataCart[1] = ["img/imgItem-2_cart.png",
+    "Men's Magenta Jacket ",
+    150,
+    "Red",
+    "M",
+    1
+];
 // const card1 = new GoodsList([
 //     new Good('ProductNew', 100, 'New descripctio', 'img/imgCard-2.png'),
 //     new Good('ProductNew123', 100, 'New descripctio123', 'img/imgCard-3.png'),
@@ -132,7 +151,17 @@ arrayData[5] = ["Green Filafil Shirt",
 // const a = arrayData[1].toString();
 // console.log(a);
 
-
+//Начал третье задание 
+// fetch('https://raw.githubusercontent.com/gjohn85456/newRepository/main/thing2.json')
+//     .then((response) => {
+//         return response.json();
+//     })
+//     .then((response) => {
+//         console.log(response);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     })
 /**
  * Функция преобразует данные в массив объектов Goods
  * @param {*} array на вход подается массив данных необходимый для формирования карточки товара
@@ -140,22 +169,39 @@ arrayData[5] = ["Green Filafil Shirt",
  */
 function createGoods(array) {
     const arrayGoods = [];
+    // array.forEach(elementArray => {
+    //     const [title, price, description, urlImg] = elementArray;
+    //     arrayGoods.push(new Good(title, price, description, urlImg));      
+
+    // });
+
+    //Реализовано формирование каточек в index.html
+    //и фомирование каточки на странице Cart.html(корзина)
     array.forEach(elementArray => {
-        const [title, price, description, urlImg] = elementArray;
-        arrayGoods.push(new Good(title, price, description, urlImg));
-        //Пока не реализовал формирование карточки в cart.html
-        // if (elementArray.leght === 4) {
-        //     const [title, price, description, urlImg] = elementArray;
-        //     arrayGoods.push(new Good(title, price, description, urlImg));
-        // } else if (elementArray.leght === 4) {
-        //     const [urlImg, title, price, color, size, quantity] = elementArray;
-        //     arrayGoods.push(new GoodInCart(urlImg, title, price, color, size, quantity));
-        // }
+        if (elementArray.length === 4) {
+            const [title, price, description, urlImg] = elementArray;
+            arrayGoods.push(new Good(title, price, urlImg, description));
+        } else if (elementArray.length != 4) {
+            const [urlImg, title, price, color, size, quantity] = elementArray;
+            arrayGoods.push(new GoodInCart(title, price, urlImg, color, size, quantity));
+        }
     });
     return arrayGoods;
 }
+//Так как скрипт подключен к двум страницам тут выполняется проверка
+//и происходит выполнение нужной части кода на соответствующей странице
+//проверка основана на наличие класса на странице
+let containerGoods = document.querySelector('.b-catalog');
+let containerInCart = document.querySelector('.mainCart_left');
 
-const card1 = new GoodsList(createGoods(arrayData));
+if (containerGoods !== null) {
+    const card1 = new GoodsList(createGoods(arrayData), containerGoods);
+    card1.renderGoodList();
+}
 
 
-card1.renderGoodList();
+if (containerInCart !== null) {
+    const card2 = new GoodsList(createGoods(arrayDataCart), containerInCart);
+    card2.renderGoodList();
+    card2.totalAmount();
+}
